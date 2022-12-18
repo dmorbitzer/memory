9afcbaedd8b6c70677d93575da56b0bdbd43e3bb
morbitzerd <d.morbitzer@techdivision.com>
2022-12-18 14:00:20 +0100

    Update packages

--- src/main/java/de/fom/memory/repository/CardRepository.java
+++
@@ -1,7 +0,0 @@
-package de.fom.memory.repository;
-
-import de.fom.memory.entity.Card;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CardRepository extends JpaRepository<Card, Long> {
-}
