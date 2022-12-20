d817945676daf97c690d41771b669117f876ed8f
morbitzerd <d.morbitzer@techdivision.com>
2022-12-20 15:45:45 +0100

    WA-3: Add integration test for CardSetController

---
+++ src/test/java/de/fom/webApp/integration/controller/CardSetControllerTest.java
@@ -0,0 +1,53 @@
+package de.fom.webApp.integration.controller;
+
+import de.fom.webApp.db.repository.CardSetRepository;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.File;
+import java.nio.file.Files;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.aMapWithSize;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class CardSetControllerTest {
+    @Autowired
+    private CardSetRepository cardSetRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @SqlGroup({
+            @Sql(value = "classpath:empty/reset.sql", executionPhase = BEFORE_TEST_METHOD),
+            @Sql(value = "classpath:init/memory-data.sql", executionPhase = BEFORE_TEST_METHOD)
+    })
+    void testLoadAllCardSets() throws Exception
+    {
+        final File jsonFile = new ClassPathResource("content/load-all-user-content.json").getFile();
+        final String loadAllUsersContent = Files.readString(jsonFile.toPath());
+
+        this.mockMvc.perform(get("/api/cardSets"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON));
+    }
+}
