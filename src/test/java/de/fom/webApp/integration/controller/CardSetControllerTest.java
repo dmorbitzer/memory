595145df9290e33e70b54211c5d7f63b7c67eb72
dstumpe <70664704+dstumpe@users.noreply.github.com>
2022-12-21 11:09:50 +0100

    WA-3: Write Test

--- src/test/java/de/fom/webApp/integration/controller/CardSetControllerTest.java
+++ src/test/java/de/fom/webApp/integration/controller/CardSetControllerTest.java
@@ -20,22 +20,118 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SqlGroup({
+        @Sql(value = "classpath:empty/reset.sql", executionPhase = BEFORE_TEST_METHOD),
+        @Sql(value = "classpath:init/memory-data.sql", executionPhase = BEFORE_TEST_METHOD)
+})
 public class CardSetControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    @SqlGroup({
-            @Sql(value = "classpath:empty/reset.sql", executionPhase = BEFORE_TEST_METHOD),
-            @Sql(value = "classpath:init/memory-data.sql", executionPhase = BEFORE_TEST_METHOD)
-    })
     void testLoadAllCardSets() throws Exception {
         this.mockMvc.perform(get("/api/cardSets"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(APPLICATION_JSON));
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(jsonPath("$.content.[0].name").value("Harry Potter"))
+                .andExpect(jsonPath("$.pageable.pageSize").value(10))
+                .andExpect(jsonPath("$.pageable.pageNumber").value(0))
+                .andExpect(jsonPath("$.last").value(true));
     }
 
+    @Test
+    void testLoadAllCardSetsWithPageParameter() throws Exception {
+        this.mockMvc.perform(
+                get("/api/cardSets")
+                        .param("page","6")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(jsonPath("$.pageable.pageNumber").value(6));
+    }
 
+    @Test
+    void testLoadAllCardSetsWithPageSizeParameter() throws Exception {
+        this.mockMvc.perform(
+                        get("/api/cardSets")
+                                .param("pageSize","6")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(jsonPath("$.pageable.pageSize").value(6));
+    }
 
+    @Test
+    void testLoadAllCardSetsWithPageParameterBogus() throws Exception {
+        this.mockMvc.perform(
+                        get("/api/cardSets")
+                                .param("page","Dobby")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(jsonPath("$.pageable.pageNumber").value(0));
+    }
+    @Test
+    void testLoadAllCardSetsWithPageSizeParameterBogus() throws Exception {
+        this.mockMvc.perform(
+                        get("/api/cardSets")
+                                .param("pageSize","Freedom")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(jsonPath("$.pageable.pageSize").value(10));
+    }
+
+    @Test
+    void testSearchCardSetsWithParam() throws Exception{
+        this.mockMvc.perform(
+                get("/api/searchCardSets")
+                        .param("searchParam", "Harry Potter")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(jsonPath("$.content.[0].name").value("Harry Potter"));
+
+    }
+    @Test
+    void testSearchCardSetsWithTags() throws Exception{
+        this.mockMvc.perform(
+                        get("/api/searchCardSets")
+                                .param("tags","Zauberei")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(jsonPath("$.content.[0].tags").value("Film;Zauberei;Magie;Fantasy"));
+    }
+
+    @Test
+    void testSearchCardSetsWithParamBogus() throws Exception{
+        this.mockMvc.perform(
+                        get("/api/searchCardSets")
+                                .param("searchParam", "Aragorn")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(jsonPath("$.content.[0].name").doesNotExist());
+
+    }
+    @Test
+    void testSearchCardSetsWithTagsBogus() throws Exception{
+        this.mockMvc.perform(
+                        get("/api/searchCardSets")
+                                .param("tags","Mein Schat")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(jsonPath("$.content.[0].tags").doesNotExist());
+    }
 }
