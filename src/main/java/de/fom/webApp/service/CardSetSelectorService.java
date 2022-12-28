db491705e1f8975437e223b6b84c16ac79f86f11
dstumpe <70664704+dstumpe@users.noreply.github.com>
2022-12-28 18:31:57 +0100

    WA-10: Implemented CardSetSelectorService

---
+++ src/main/java/de/fom/webApp/service/CardSetSelectorService.java
@@ -0,0 +1,77 @@
+package de.fom.webApp.service;
+
+import de.fom.webApp.db.entity.CardSet;
+import de.fom.webApp.db.repository.CardSetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+@Service
+public class CardSetSelectorService {
+    /**
+     * CardSetRepository cardSetRepository
+     */
+    private final CardSetRepository cardSetRepository;
+    /**
+     * PaginationService paginationService
+     */
+    private final PaginationService paginationService;
+    /**
+     *
+     * @param cardSetRepository CardSetRepository
+     * @param paginationService PaginationService
+     */
+    @Autowired
+    public CardSetSelectorService(
+            CardSetRepository cardSetRepository,
+            PaginationService paginationService
+    ) {
+        this.cardSetRepository = cardSetRepository;
+        this.paginationService = paginationService;
+    }
+
+    /**
+     * @param cardSetId String
+     * @param page String
+     * @param pageSize String
+     */
+    public Page<CardSet> selectCardSetById (
+            String cardSetId,
+            String page,
+            String pageSize
+    ) {
+        PageRequest pageRequest = this.paginationService.createPageable(
+                page,
+                pageSize
+        );
+        long cardSetIdNumber = this.paginationService.numberParser(cardSetId);
+
+        List<CardSet> cardSets = this.cardSetRepository.findAll();
+
+        List<Long> ids = new LinkedList<Long>();
+
+        for (int i = 0; i < cardSets.size();i++) {
+           ids.add(cardSets.get(i).getId());
+        }
+
+        ids.sort(Comparator.naturalOrder());
+
+        if(!ids.contains(cardSetIdNumber)) {
+            cardSetIdNumber = ids.get(0);
+        }
+
+        cardSetId = Long.toString(cardSetIdNumber);
+
+        return this.cardSetRepository.findById(
+                cardSetId,
+                pageRequest
+        );
+    }
+
+
+
+}
