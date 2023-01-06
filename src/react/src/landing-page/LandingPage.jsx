22fe7ab37aafc1b8a70b27cb5343121035ac1fcb
bhecker <benji.link@outlook.de>
2023-01-06 15:41:27 +0100

    Added signup component and inserted it in landing page component

--- src/react/src/landing-page/LandingPage.jsx
+++ src/react/src/landing-page/LandingPage.jsx
@@ -1,25 +1,38 @@
 import * as React from 'react';
-import logo from '../logo.svg';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Signup from '../sign-up/Signup';
+import './LandingPage.css';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 function LandingPage() {
-    return(
-        <body className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <p>
-                Edit
-                <code> src/App.js </code>
-                and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-            </a>
-        </body>
-    );
+  return (
+    <div className="box-container">
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={0.5}>
+          <Grid item xs={5}>
+            <Item>
+              <Signup />
+            </Item>
+          </Grid>
+          <Grid item xs={7}>
+            <Item>
+              <img src="../assets/pensioner-playing-computer-game.png" alt="Hide the pain" />
+            </Item>
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
+  );
 }
 
 export default LandingPage;
