275da13f825d84a6a12c9700667ad6823ab73fda
bhecker <benji.link@outlook.de>
2023-01-12 18:03:47 +0100

    Fixed lint errors

--- src/react/src/App.jsx
+++ src/react/src/App.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import './App.css';
-import {Route, BrowserRouter, Routes} from 'react-router-dom';
-import Navbar from "./navbar/Navbar";
-import Footer from "./footer/Footer";
-import LandingPage from "./landing-page/LandingPage";
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import Navbar from './navbar/Navbar';
+import Footer from './footer/Footer';
+import LandingPage from './landing-page/LandingPage';
 
 function App() {
-    return (
-        <div className="App">
-            <header>
-                <Navbar/>
-            </header>
-            <body>
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<LandingPage/>}/>
-                        <Route path="*" element={<LandingPage/>}/>
-                    </Routes>
-                </BrowserRouter>
-            </body>
-            <footer>
-                <Footer/>
-            </footer>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header>
+        <Navbar />
+      </header>
+      <body>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="*" element={<LandingPage />} />
+          </Routes>
+        </BrowserRouter>
+      </body>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 }
 
 export default App;
--- src/react/src/footer/Footer.jsx
+++ src/react/src/footer/Footer.jsx
@@ -3,25 +3,25 @@ import PsychologyIcon from '@mui/icons-material/Psychology';
 import './Footer.css';
 
 function Footer() {
-    return (
-        <div className="footer-container">
-            <div className="logo-container">
-                <PsychologyIcon sx={{display: {xs: 'none', md: 'flex'}, mr: 1, fontSize: 45}}/>
-            </div>
-            <div className="button-container">
-                <button>
-                    <span>Legal Notice</span>
-                </button>
-                <button>
-                    <span>Terms and Conditions</span>
-                </button>
-                <button>
-                    <span>Imprint</span>
-                </button>
-            </div>
-            <span className="copyright">Copyright &copy; 2022-2023 Memorizer Inc.</span>
-        </div>
-    );
+  return (
+    <div className="footer-container">
+      <div className="logo-container">
+        <PsychologyIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1, fontSize: 45 }} />
+      </div>
+      <div className="button-container">
+        <button type="button">
+          <span>Legal Notice</span>
+        </button>
+        <button type="button">
+          <span>Terms and Conditions</span>
+        </button>
+        <button type="button">
+          <span>Imprint</span>
+        </button>
+      </div>
+      <span className="copyright">Copyright &copy; 2022-2023 Memorizer Inc.</span>
+    </div>
+  );
 }
 
 export default Footer;
