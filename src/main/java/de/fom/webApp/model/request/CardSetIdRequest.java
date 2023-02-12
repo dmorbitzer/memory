7a49f918b1e490d89dd5cb47f189cfaa7352578b
Dominik Stumpe <dominik-stumpe@web.de>
2023-02-12 10:51:49 +0100

    WA-10: added request object

--- src/main/java/de/fom/webApp/model/request/CardSetIdRequest.java
+++ src/main/java/de/fom/webApp/model/request/CardSetIdRequest.java
@@ -1,13 +1,23 @@
 package de.fom.webapp.model.request;
 
 public class CardSetIdRequest {
-
+    /**
+     * String
+     */
     private String cardSetId;
 
+    /**
+     *
+     * @return cardSetId
+     */
     public String getCardSetId() {
         return cardSetId;
     }
 
+    /**
+     *
+     * @param cardSetId String
+     */
     public void setCardSetId(String cardSetId) {
         this.cardSetId = cardSetId;
     }
