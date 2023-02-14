7cd83a914210f4881c305dd35eac6e78258c751f
bhecker <benji.link@outlook.de>
2023-02-14 15:50:54 +0100

    Changed dialog styling

--- src/react/src/back-button/BackButton.jsx
+++ src/react/src/back-button/BackButton.jsx
@@ -5,6 +5,8 @@ import Button from '@mui/material/Button';
 import {
   Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle,
 } from '@mui/material';
+import FlagCircleRoundedIcon from '@mui/icons-material/FlagCircleRounded';
+import Box from '@mui/material/Box';
 
 function BackButton() {
   const [open, setOpen] = useState(false);
@@ -27,7 +29,13 @@ function BackButton() {
       <Button
         onClick={handleClickOpen}
       >
-        <ArrowBackOutlinedIcon sx={{ color: 'white', fontSize: '30px' }} />
+        <ArrowBackOutlinedIcon sx={{
+          color: 'white',
+          fontSize: '30px',
+          border: '3px solid white',
+          borderRadius: '50px',
+        }}
+        />
       </Button>
       <Dialog
         open={open}
@@ -35,7 +43,17 @@ function BackButton() {
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <DialogTitle id="alert-dialog-title">
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            pt: '2rem',
+          }}
+          color="secondary.main"
+        >
+          <FlagCircleRoundedIcon fontSize="large" />
+        </Box>
+        <DialogTitle sx={{ textAlign: 'center', fontSize: '26px' }} id="alert-dialog-title">
           Do you really want to forfeit?
         </DialogTitle>
         <DialogContent>
@@ -43,10 +61,10 @@ function BackButton() {
             Your progress will not be saved. Do you still want to end this round?
           </DialogContentText>
         </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Disagree</Button>
-          <Button onClick={handleAgree} autoFocus>
-            Agree
+        <DialogActions sx={{ display: 'flex', justifyContent: 'center', pb: '2rem' }}>
+          <Button variant="contained" onClick={handleClose}>Keep playing</Button>
+          <Button variant="outlined" onClick={handleAgree} autoFocus>
+            forfeit
           </Button>
         </DialogActions>
       </Dialog>
